724dc7d4a0cd0e05221a9dc2cc226574f5f97d1b
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-16 08:27:18 +0300

    change the migrations

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://memory:MEM99ory@localhost/blogs'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://memory:MEM99ory@localhost/bloggs'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You-will-never-guess'
    
     UPLOADED_PHOTOS_DEST ='app/static/photos'
